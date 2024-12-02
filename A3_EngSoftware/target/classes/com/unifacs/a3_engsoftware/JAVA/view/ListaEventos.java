fdf5817a577909250cccdc14447e55ce0bd3a49a
Álex Lima <alex.lima323966@gmail.com>
2024-12-02 10:41:01 -0300

    feat: Ajustando view ListarEventos

--- A3_EngSoftware/target/classes/com/unifacs/a3_engsoftware/JAVA/view/ListaEventos.java
+++ A3_EngSoftware/target/classes/com/unifacs/a3_engsoftware/JAVA/view/ListaEventos.java
@@ -225,7 +225,7 @@ public class ListaEventos extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ListaEventos().setVisible(true);
+                    
             }
         });
     }
